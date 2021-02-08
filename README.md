9313ee0ce78e7e67bd1251d666f748013a0eaec3
miloyip <miloyip@gmail.com>
2021-02-09 00:41:43 +0800

    feat: initial commit
    Toggle selction
    Expand selection
    Chronology
    Edit title

---
+++ README.md
@@ -0,0 +1,42 @@
+# Game ⨯ Tech
+
+Visualize game related technology for knowledge management.
+
+## Target users
+
+* Game developers
+* Researchers
+* Students
+
+## Features
+
+* Explore technology and games which employ such
+* Chronological view mode
+* Share a view with URL
+* Serverless for easy contribution and merging
+
+## Schema
+
+Nodes are array of objects with following keys (asterisk are must exist):
+
+| Key         | Type                      | Description                                                  |
+| ----------- | ------------------------- | ------------------------------------------------------------ |
+| id*         | string                    | Unique idenfitifer in title case                             |
+| abbr        | string                    | Abbreviation                                                 |
+| type        | string                    | "category" or "technique" or "game"                          |
+| year        | number                    | Year of invention or publicaiton                             |
+| synopsis    | string                    | Concise description                                          |
+| author      | string or array of string | Inventor(s) or author(s)                                     |
+| link        | string or array of string | links of publication, wikipedia or other related information | 
+| title       | string                    | Title of the first publication which reveals this            | 
+| conference  | string                    | Abbreviation of conference name, e.g. GDC, SIGGRAPH          |
+| journal     | string                    | Abbreviation of journal name, e.g. JCGT                      |
+| contributor | string or array of string | github usernames of whom contribute to this node             | 
+
+Edge are arrays of objects with following keys (asterisk are must exist):
+
+| Key       | Type     | Description           |  
+| --------- | -------- | --------------------- |  
+| source*   | string   | Source node id        |  
+| target*   | string   | Target node id        |  
+| type      | string   | "include" or "derive" |  
---
+++ tech/tech_node.js
@@ -0,0 +1,650 @@
+nodes = [
+    {
+        "id":"Technology",
+    },
+    {
+        "id":"Mathematics"
+    },
+    {
+        "id":"Algebra"
+    },
+    {
+        "id":"Linear Algebra"
+    },
+    {
+        "id":"Vector"
+    },
+    {
+        "id":"Matrix"
+    },
+    {
+        "id":"Quaternion"
+    },
+    {
+        "id":"Dual Quaternion"
+    },
+    {
+        "id":"Grasmann Algebra"
+    },
+    {
+        "id":"Calculus"
+    },
+    {
+        "id":"Vector Calculus"
+    },
+    {
+        "id":"Geometry"
+    },
+    {
+        "id":"Computer Science"
+    },
+    {
+        "id":"Algorithm"
+    },
+    {
+        "id":"Data Structure"
+    },
+    {
+        "id":"Computer Architecture"
+    },
+    {
+        "id":"Computational Geometry"
+    },
+    {
+        "id":"Numerical Method"
+    },
+    {
+        "id":"Root Finding"
+    },
+    {
+        "id":"Bisection Method"
+    },
+    {
+        "id":"Newton's Method"
+    },
+    {
+        "id":"Runge-Kutta Method"
+    },
+    {
+        "id":"Differential Equation Solving"
+    },
+    {
+        "id":"Euler Method"
+    },
+    {
+        "id":"Verlet Integration"
+    },
+    {
+        "id":"Platform"
+    },
+    {
+        "id":"Personal Computer",
+        "abbr":"PC"
+    },
+    {
+        "id":"Windows"
+    },
+    {
+        "id":"MacOS"
+    },
+    {
+        "id":"Linux"
+    },
+    {
+        "id":"Mobile Device"
+    },
+    {
+        "id":"iOS"
+    },
+    {
+        "id":"Android"
+    },
+    {
+        "id":"Video Game Console"
+    },
+    {
+        "id":"PlayStation"
+    },
+    {
+        "id":"Xbox"
+    },
+    {
+        "id":"Portable Game Console"
+    },
+    {
+        "id":"Nintendo Switch"
+    },
+    {
+        "id":"Programming"
+    },
+    {
+        "id":"Programming Paradigm"
+    },
+    {
+        "id":"Procedural Programming"
+    },
+    {
+        "id":"Object-oriented Programming",
+        "abbr":"OOP"
+    },
+    {
+        "id":"Functional Programming",
+        "abbr":"FP"
+    },
+    {
+        "id":"Event-driven Programming"
+    },
+    {
+        "id":"Programming Language"
+    },
+    {
+        "id":"Native Programming Language"
+    },
+    {
+        "id":"Assembly"
+    },
+    {
+        "id":"C"
+    },
+    {
+        "id":"C++"
+    },
+    {
+        "id":"Objective-C"
+    },
+    {
+        "id":"Swift"
+    },
+    {
+        "id":"Rust"
+    },
+    {
+        "id":"Java"
+    },
+    {
+        "id":"C#"
+    },
+    {
+        "id":"Scripting Programming Language"
+    },
+    {
+        "id":"Lua"
+    },
+    {
+        "id":"Python"
+    },
+    {
+        "id":"Shader Programming Language"
+    },
+    {
+        "id":"High-Level Shader Language",
+        "abbr":"HLSL"
+    },
+    {
+        "id":"OpenGL Shading Language",
+        "abbr":"GLSL"
+    },
+    {
+        "id":"Metal Shading Language",
+        "abbr":"MSL"
+    },
+    {
+        "id":"Software Engineering"
+    },
+    {
+        "id":"Software Design"
+    },
+    {
+        "id":"Unified Modeling Language",
+        "abbr":"UML"
+    },
+    {
+        "id":"Design Pattern"
+    },
+    {
+        "id":"Debugging"
+    },
+    {
+        "id":"Testing"
+    },
+    {
+        "id":"Profiling"
+    },
+    {
+        "id":"Verson Control"
+    },
+    {
+        "id":"Graphics"
+    },
+    {
+        "id":"Graphics API"
+    },
+    {
+        "id":"Direct3D"
+    },
+    {
+        "id":"OpenGL"
+    },
+    {
+        "id":"OpenGL ES"
+    },
+    {
+        "id":"Vulkan"
+    },
+    {
+        "id":"Metal"
+    },
+    {
+        "id":"Modeling"
+    },
+    {
+        "id":"Curve"
+    },
+    {
+        "id":"Curved Surface"
+    },
+    {
+        "id":"Mesh"
+    },
+    {
+        "id":"Triangle Mesh"
+    },
+    {
+        "id":"Digital Elevation Model",
+        "abbr":"DEM"
+    },
+    {
+        "id":"Voxel"
+    },
+    {
+        "id":"Animation"
+    },
+    {
+        "id":"Key-frame Animation"
+    },
+    {
+        "id":"Particle System"
+    },
+    {
+        "id":"Skinning"
+    },
+    {
+        "id":"Morphing"
+    },
+    {
+        "id":"Animation Tree"
+    },
+    {
+        "id":"Motion Matching"
+    },
+    {
+        "id":"Rendering"
+    },
+    {
+        "id":"Render Pipeline"
+    },
+    {
+        "id":"Scene Management"
+    },
+    {
+        "id":"Camera"
+    },
+    {
+        "id":"Lighting"
+    },
+    {
+        "id":"Global Illimulation",
+        "abbr":"GI"
+    },
+    {
+        "id":"Ray Tracing",
+        "author":"John Turner Whitted",
+        "title":"An improved illumination model for shaded display.",
+        "conference":"PACMCGIT",
+        "year":1979
+    },
+    {
+        "id":"Path Tracing"
+    },
+    {
+        "id":"Radiosity"
+    },
+    {
+        "id":"Photon Mapping"
+    },
+    {
+        "id":"Shadow"
+    },
+    {
+        "id":"Shadow Volume",
+        "author":"Franklin Crow",
+        "title":"Shadow Algorithms for Computer Graphics",
+        "conference":"SIGGRAPH",
+        "year":1977
+    },
+    {
+        "id":"Stencil Shadow Volume",
+        "author":"Tim Heidmann",
+        "title":"Real Shadows, Real Time",
+        "year":1991
+    },
+    {
+        "id":"Carmack's Reverse",
+        "author":"John Carmck",
+        "year":2000
+    },
+    {
+        "id":"Shadow Map",
+        "author":"Lance Joseph Williams",
+        "title":"Casting curved shadows on curved surfaces",
+        "conference":"PACMCGIT",
+        "year":1978
+    },
+    {
+        "id":"Shadow Map Projection Schemes"
+    },
+    {
+        "id":"Trapezoidal Shadow Map",
+        "abbr":"TSM",
+        "author":[
+            "Tobias Martin",
+            "Tiow-Seng Tan"
+        ],
+        "title":"Anti-aliasing and Continuity with Trapezoidal Shadow Maps",
+        "conference":"EGSR",
+        "year":2004
+    },
+    {
+        "id":"Parallel-Split Shadow Map",
+        "abbr":"PSSM",
+        "author":[
+            "Fan Zhang",
+            "Hanqiu Sun",
+            "Leilei Xu",
+            "Lee Kit Lun"
+        ],
+        "title":"Parallel-split shadow maps for large-scale virtual environments",
+        "conference":"VRCAI",
+        "year":2006
+    },
+    {
+        "id":"Cascaded Shadow Map",
+        "abbr":"CSM",
+        "author":"Rouslan Dimitrov",
+        "link":"http://developer.download.nvidia.com/SDK/10.5/opengl/src/cascaded_shadow_maps/doc/cascaded_shadow_maps.pdf",
+        "year":2007
+    },
+    {
+        "id":"Light Space Perspective Shadow Map",
+        "abbr":"LSPSM",
+        "author":[
+            "Michael Wimmer",
+            "Daniel Scherzer",
+            "Werner Purgathofer"
+        ],
+        "conference":"EGSR",
+        "year":2004
+    },
+    {
+        "id":"Shadow Map Filtering Schemes"
+    },
+    {
+        "id":"Percentage Closer Filtering",
+        "abbr":"PCF",
+        "author":[
+            "William T. Reeves",
+            "David H. Salesin",
+            "Robert L. Cook"
+        ],
+        "title":"Rendering Antialiased Shadows with Depth Maps",
+        "conference":"SIGGRAPH",
+        "year":1987
+    },
+    {
+        "id":"Variance Shadow Map",
+        "abbr":"VSM",
+        "author":[
+            "William Donnelly",
+            "Andrew Lauritzen"
+        ],
+        "title":"Variance shadow maps",
+        "conference":"I3D",
+        "year":2006
+    },
+    {
+        "id":"Soft Shadow Shadow Map"
+    },
+    {
+        "id":"Percentage Closer Soft Shadow",
+        "abbr":"PCSS"
+    },
+    {
+        "id":"Screen-space Soft Shadow",
+        "abbr":"SSSS"
+    },
+    {
+        "id":"Variance Soft Shadow Map",
+        "abbr":"VSSM",
+        "author":[
+            "Baoguang Yang",
+            "Zhao Dong",
+            "Jieqing Feng",
+            "Hans-Peter Seidel",
+            "Jan Kautz"
+        ],
+        "title":"Variance Soft Shadow Mapping",
+        "conference":"Pacific Graphics",
+        "year":2010
+    },
+    {
+        "id":"Texture"
+    },
+    {
+        "id":"Material"
+    },
+    {
+        "id":"Post-Processing"
+    },
+    {
+        "id":"Audio"
+    },
+    {
+        "id":"Human Computer Interaction",
+        "abbr":"HCI"
+    },
+    {
+        "id":"Physics"
+    },
+    {
+        "id":"Collision Detection",
+        "abbr":"CD"
+    },
+    {
+        "id":"Broad-phase Collision Detection"
+    },
+    {
+        "id":"Narrow-phase Collision Detection"
+    },
+    {
+        "id":"Rigid Body Dynamics"
+    },
+    {
+        "id":"Soft Body Dynamics"
+    },
+    {
+        "id":"Rope Simulation"
+    },
+    {
+        "id":"Cloth Simulation"
+    },
+    {
+        "id":"Fluid Simulation"
+    },
+    {
+        "id":"Eulerian Fluid Simulation"
+    },
+    {
+        "id":"Stable Fluid"
+    },
+    {
+        "id":"Lagrangian Fluid Simulation"
+    },
+    {
+        "id":"Smoothed Particle Hydrodynamics",
+        "abbr":"SPH"
+    },
+    {
+        "id":"Hybrid Fluid Simulation"
+    },
+    {
+        "id":"Particle-In-Cell Method",
+        "abbr":"PIC"
+    },
+    {
+        "id":"Vehicle Simulation"
+    },
+    {
+        "id":"Fracture Simulation"
+    },
+    {
+        "id":"Physics Engine"
+    },
+    {
+        "id":"Commercial Physics Engine"
+    },
+    {
+        "id":"PhysX"
+    },
+    {
+        "id":"Havok"
+    },
+    {
+        "id":"Open Source Physics Engine"
+    },
+    {
+        "id":"Bullet"
+    },
+    {
+        "id":"Open Dynamics Engine",
+        "abbr":"ODE"
+    },
+    {
+        "id":"Box2D"
+    },
+    {
+        "id":"Artificial Intelligence"
+    },
+    {
+        "id":"Game Tree"
+    },
+    {
+        "id":"Minimax Search"
+    },
+    {
+        "id":"Alpha-beta Prunning"
+    },
+    {
+        "id":"Behavior Modeling"
+    },
+    {
+        "id":"Finite State Machine",
+        "abbr":"FSM"
+    },
+    {
+        "id":"Behavior Tree"
+    },
+    {
+        "id":"Navigation"
+    },
+    {
+        "id":"Path Finding"
+    },
+    {
+        "id":"Obstacle Avoidance"
+    },
+    {
+        "id":"Machine Learning"
+    },
+    {
+        "id":"Supervised Learning"
+    },
+    {
+        "id":"Unsupervised Learning"
+    },
+    {
+        "id":"Reinforcement Learning"
+    },
+    {
+        "id":"Natural Language Processing",
+        "abbr":"NLP"
+    },
+    {
+        "id":"Gameplay"
+    },
+    {
+        "id":"Multiplayer"
+    },
+    {
+        "id":"Game Engine"
+    },
+    {
+        "id":"Commercial Game Engine"
+    },
+    {
+        "id":"Unreal Engine",
+        "abbr":"UE"
+    },
+    {
+        "id":"Unity"
+    },
+    {
+        "id":"CryEngine"
+    },
+    {
+        "id":"Open Source Game Engine"
+    },
+    {
+        "id":"Cocos2dx"
+    },
+    {
+        "id":"Godot"
+    },
+    {
+        "id":"Proprietary Game Engine"
+    },
+    {
+        "id":"Anvil"
+    },
+    {
+        "id":"Decima"
+    },
+    {
+        "id":"Dunia"
+    },
+    {
+        "id":"Insomniac Engine"
+    },
+    {
+        "id":"Forstbite"
+    },
+    {
+        "id":"QuickSilver"
+    },
+    {
+        "id":"Content Creation"
+    },
+    {
+        "id":"Digital Content Creation",
+        "abbr":"DCC"
+    },
+    {
+        "id":"Procedural Content Generation",
+        "abbr":"PCG"
+    },
+    {
+        "id":"Photogrammetry"
+    },
+    {
+        "id":"Motion Capture"
+    }
+]
