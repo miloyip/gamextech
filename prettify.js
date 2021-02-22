f066c330bbbf902fe24ece4c8de28b403fd075c0
miloyip <miloyip@gmail.com>
2021-02-22 23:31:06 +0800

    feat: add JSON prettify script

---
+++ prettify.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const prettify = (filename) => {
+    const src = fs.readFileSync(filename, 'utf8');
+    const json = JSON.parse(src);
+    const dst = JSON.stringify(json, null, 2);
+    if (src != dst) {
+        fs.writeFileSync(filename, dst);
+        console.log(`Prettified ${filename}`);
+    }
+    return json;
+}
+
+prettify("database.json").forEach(filename => prettify(filename));
